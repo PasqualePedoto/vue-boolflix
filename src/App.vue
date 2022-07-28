b8966180e80e12c360968e7b7d25ff85e70a1040
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-28 16:23:30 +0200

    Prelevati i dati tramite query e passati come props verso il main

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,68 @@
 <template>
   <div>
     <BaseHeader />
+    <BaseMain :query-results="theMovieResults" />
   </div>
 </template>
 
 <script>
 import BaseHeader from "./components/BaseHeader.vue";
-// import axios from "axios";
+import BaseMain from "./components/BaseMain.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     BaseHeader,
+    BaseMain,
   },
   data() {
     return {
-      TheMovieResults: [],
+      // Definiamo l'Uri base e le specifiche
+      baseUri: "https://api.themoviedb.org/3",
+      searchMovies: "/search/movie",
+
+      // Parametri da passare alla query
+      language: "it-IT",
+      query: "anelli",
+      api_key: "94e919df475d5245ad0600c98048db21",
+
+      //Array che manterrà i risultati della query
+      theMovieResults: [],
     };
   },
-  methods: {},
+  computed: {
+    // La computed qui definita tiene traccia
+    // delle Uri modificate in funzione di cosa cerchiamo
+    moviesUri() {
+      return this.baseUri + this.searchMovies;
+    },
+  },
+  methods: {
+    getMoviesFromTheMovieDB() {
+      // Definiamo un set di parametri da passare alla
+      // chiamata verso l'API
+      const config = {
+        params: {
+          api_key: this.api_key,
+          query: this.query,
+          language: this.language,
+        },
+      };
+
+      axios
+        .get(this.moviesUri, config)
+        .then((res) => {
+          this.theMovieResults = res.data.results;
+        })
+        .catch((err) => {
+          console.err(err);
+        });
+    },
+  },
+  mounted() {
+    this.getMoviesFromTheMovieDB();
+  },
 };
 </script>
 
---
+++ src/components/BaseMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="container">
+    <ul>
+      <li v-for="(result, index) in QueryResults" :key="index">
+        <ul>
+          <li>Original_Title: {{ result.original_title }}</li>
+          <li>Title: {{ result.title }}</li>
+          <li>Language: {{ result.original_language }}</li>
+          <li>Title: {{ result.vote_average }}</li>
+        </ul>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "BaseMain",
+  props: {
+    QueryResults: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
