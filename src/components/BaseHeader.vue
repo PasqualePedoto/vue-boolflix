843376b6e841bd1a9a82492006d417bfd7382c1a
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-28 17:27:08 +0200

    Creato ed aggiunto nuovo componente: BaseSearchBar.vue

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,30 +1,15 @@
 <template>
   <header class="container d-flex align-items-center justify-content-between">
     <h1>Boolfix</h1>
-    <div class="d-flex align-items-center justify-content-center">
-      <input type="text" class="form-control me-2" placeholder="Cerca qui" @keyup.enter="emitQuery" @keyup.esc="cancelQuery" v-model="query" />
-      <button role="button" class="btn btn-primary" @click="emitQuery">Click!</button>
-    </div>
+    <BaseSearchBar />
   </header>
 </template>
 
 <script>
+import BaseSearchBar from "./BaseSearchBar.vue";
 export default {
   name: "BaseHeader",
-  data() {
-    return {
-      query: "",
-    };
-  },
-  methods: {
-    emitQuery() {
-      if (this.query === "") return;
-      else this.$emit("search-query", this.query);
-    },
-    cancelQuery() {
-      this.query = "";
-    },
-  },
+  components: { BaseSearchBar },
 };
 </script>
 
---
+++ src/components/BaseSearchBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="d-flex align-items-center justify-content-center">
+    <input type="text" class="form-control me-2" placeholder="Cerca qui" @keyup.enter="emitQuery" @keyup.esc="cancelQuery" v-model="query" />
+    <button role="button" class="btn btn-primary" @click="emitQuery">Click!</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "BaseSearchBar",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    emitQuery() {
+      if (this.query === "") return;
+      else this.$emit("search-query", this.query);
+    },
+    cancelQuery() {
+      this.query = "";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
