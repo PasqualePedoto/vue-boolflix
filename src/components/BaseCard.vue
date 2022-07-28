b56847b22b272305a11a02c26a42433abf071a40
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-28 16:57:35 +0200

    Aggiunta la dinamicità della bandiera italia ed inglese

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -3,7 +3,15 @@
     <ul>
       <li>Original_Title: {{ result.original_title }}</li>
       <li>Title: {{ result.title }}</li>
-      <li>Language: {{ result.original_language }}</li>
+      <li v-if="result.original_language === 'it' || result.original_language === 'en'">
+        <div class="d-flex align-items-center">
+          <p>Language:</p>
+          <figure class="m-0">
+            <img :src="nationFlag(result.original_language)" :alt="nationFlag(result.original_language)" />
+          </figure>
+        </div>
+      </li>
+      <li v-else>{{ result.original_language }}</li>
       <li>Title: {{ result.vote_average }}</li>
     </ul>
   </section>
@@ -14,6 +22,11 @@ export default {
   name: "BaseCard",
   props: {
     result: Object,
+  },
+  methods: {
+    nationFlag(string) {
+      return require(`../assets/image/${string}.png`);
+    },
   },
 };
 </script>
